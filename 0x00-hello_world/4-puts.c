9d327862d9a91b8de4b43021e48b4ece836318c1
EzekielDamz <ezekieldammy1@gmail.com>
2023-07-13 15:54:56 -0700

    printf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
+int main(void)
 /**
-*main - main function
+*main - entry point
 *return : always 0 (success)
 */
-int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle")
 	return (0);
-{
+}
