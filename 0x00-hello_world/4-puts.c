b22c319ff51cc7cc785392aca19602cb71d618dc
EzekielDamz <ezekieldammy1@gmail.com>
2023-07-13 15:18:56 -0700

    Hello, C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
+/**
+*main - main function
+*return : always 0 (success)
+*/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 {
