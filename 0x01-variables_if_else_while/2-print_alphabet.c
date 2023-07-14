0509ab3b56edcb2bebd47c81d2b850051b2f4743
EzekielDamz <ezekieldammy1@gmail.com>
2023-07-14 10:41:01 -0700

    I sometimes suffer from

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -8,13 +8,12 @@
 
 int main(void)
 {
-	int lowerCase = 'a';
-
-        while (lowerCase <= 'z')
-	{
-		putchar(lowerCase);
-		lowerCase += 1;
-	}
-	putchar('\n');
-	return (0);
+int lowerCase = 'a'
+while (lowerCase <= 'z')
+{
+putchar(lowerCase);
+lowerCase += 1;
+}
+putchar('\n');
+return (0);
 }
