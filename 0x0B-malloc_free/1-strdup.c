d9d112775581ca85d4d3e24650d594c2e0d65f55
EzekielDamz <ezekieldammy1@gmail.com>
2023-08-09 06:16:39 -0700

    malloc wahalla

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,29 +1,23 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include "main.h"
-
+#include <stdlib.h>
 /**
- * _strdup - duplicate to new memory space location
- * @str: char
- * Return: 0
+ * create_array - create array of size size and assign char c
+ * @size: size of array
+ * @c: char to assign
+ * Description: create array of size size and assign char c
+ * Return: pointer to array, NULL if fail
+ *
  */
-
-char *_strdup(char *str)
+char *create_array(unsigned int size, char c)
 {
-	if (str == NULL)
-	{
+	char *str;
+	unsigned int i;
+
+	str = malloc(sizeof(char) * size);
+	if (size == 0 || str == NULL)
 		return (NULL);
-	}
-	size_t len = strlen(str);
 
-	char *new_str = (char *)malloc(len + 1);
-
-	if (new_str == NULL)
-	{
-		return (NULL);
-	}
-	strcpy(new_str, str);
-
-	return (new_str);
+	for (i = 0; i < size; i++)
+		str[i] = c;
+	return (str);
 }
