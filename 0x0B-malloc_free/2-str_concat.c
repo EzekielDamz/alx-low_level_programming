9cc55d8f5cb10f0e1bee44aa3075809570575e09
EzekielDamz <ezekieldammy1@gmail.com>
2023-08-09 07:16:26 -0700

    mad oo

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -8,22 +8,20 @@
  */
 char *_strdup(char *str)
 {
-	char *aaa;
-	int i, r = 0;
+char *aaa;
+int i, r = 0;
 
-	if (str == NULL)
-		return (NULL);
-	i = 0;
-	while (str[i] != '\0')
-		i++;
+if (str == NULL)
+return (NULL);
+i = 0;
+while (str[i] != '\0')
+i++;
+aaa = malloc(sizeof(char) * (i + 1));
 
-	aaa = malloc(sizeof(char) * (i + 1));
+if (aaa == NULL)
+return (NULL);
 
-	if (aaa == NULL)
-		return (NULL);
-
-	for (r = 0; str[r]; r++)
-		aaa[r] = str[r];
-
-	return (aaa);
+for (r = 0; str[r]; r++)
+aaa[r] = str[r];
+return (aaa);
 }
