132d132e08091262334648966b97e669e59d3d75
EzekielDamz <ezekieldammy1@gmail.com>
2023-08-15 12:15:44 -0700

    updated

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -9,8 +9,8 @@
 void print_dog(struct dog *d)
 {
 	if (d)
-	{
-		if (!((*d).name))
+	{ 
+		if (!(d->name))
 			printf("Name: (nil)\n");
 		else
 			printf("Name: %s\n", d->name);
@@ -19,6 +19,6 @@ void print_dog(struct dog *d)
 		if (!(d->owner))
 			printf("Owner: (nil)\n");
 		else
-			printf("Owner: %s\n", (*d).owner);
+			printf("Owner: %s\n", d->owner);
 	}
 }
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -15,7 +15,7 @@ typedef struct dog
 	char *name;
 	float age;
 	char *owner;
-} doh_t;
+} dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 #endif
