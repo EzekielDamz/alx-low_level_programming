e5fcba2be57f25b58e4e7b6ec399df9386cfcdac
EzekielDamz <ezekieldammy1@gmail.com>
2023-08-15 02:48:39 -0700

    Create a header file that defines a macro named SIZE

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,10 +1,8 @@
 #ifndef MACRO_H
 #define MACRO_H
 /**
-  * File: 0-object_like_macro.h
-  * 
-  * Description: header file
-  */
+*File: 0-object_like_macro.h
+*Description: header file
+*/
 #define SIZE 1024
-
 #endif
