590e31b48d94886a2301505ea63cfe40ffbe41ca
EzekielDamz <ezekieldammy1@gmail.com>
2023-08-15 06:14:17 -0700

    Write a function-like macro SUM(x, y) that computes the sum of the numbers x and y

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
+
 /**
-  * Description: print out file names
-  *
-  * main- void
-  * Return: int
-  */
+ * main - prints the name of the file
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	printf("%s\n", __FILE__);
---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,10 @@
+#ifndef SUM_H
+#define SUM_H
+/**
+  * file: 4-sum.h
+  *
+  * @x: first input 
+  * @y: second input
+  */
+#define SUM(x, y) (x + y)
+#endif
