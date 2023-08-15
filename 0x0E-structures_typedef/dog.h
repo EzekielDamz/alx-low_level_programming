cc882785550272ca8fac78162f1184ae7fad8c72
EzekielDamz <ezekieldammy1@gmail.com>
2023-08-15 08:20:27 -0700

    Define a new type struct dog with the following elements

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+/**
+  * struct dog - a struct that  create a dog info
+  *
+  * @name: the name of dog
+  * @age: the age of dog
+  * @owner: owner of the dog
+  *
+  * Description: first struct with alx
+  */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
